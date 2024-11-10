4608d36745a8cf9111e55f300a77ea318d61c75b
Matt Walters <mw2gd@virginia.edu>
2024-11-10 12:10:35 -0500

    working

--- src/fullapp.cpp
+++ src/fullapp.cpp
@@ -4,7 +4,7 @@
 * Use of this source code is governed by a BSD-style license that can be
 * found in the LICENSE file.
 */
-#include "HelloWorld.h"
+#include "fullapp.h"
 #include "include/core/SkCanvas.h"
 #include "include/core/SkColor.h"
 #include "include/core/SkFont.h"
@@ -23,10 +23,12 @@
 
 using namespace sk_app;
 using skwindow::DisplayParams;
+
 Application* Application::Create(int argc, char** argv, void* platformData) {
-    return new HelloWorld(argc, argv, platformData);
+    return new FullApp(argc, argv, platformData);
 }
-HelloWorld::HelloWorld(int argc, char** argv, void* platformData)
+
+FullApp::FullApp(int argc, char** argv, void* platformData)
         : fBackendType(Window::kRaster_BackendType),
         fRotationAngle(0) {
     SkGraphics::Init();
@@ -36,38 +38,32 @@ HelloWorld::HelloWorld(int argc, char** argv, void* platformData)
     fWindow->pushLayer(this);
     fWindow->attach(fBackendType);
 }
-HelloWorld::~HelloWorld() {
+
+FullApp::~FullApp() {
     fWindow->detach();
     delete fWindow;
 }
-void HelloWorld::updateTitle() {
+
+void FullApp::updateTitle() {
     if (!fWindow) {
         return;
     }
-    SkString title("Hello World ");
-    if (Window::kRaster_BackendType == fBackendType) {
-        title.append("Raster");
-    } else {
-
-        title.append("Unknown GPU backend");
-    }
+    SkString title("FullApp");
     fWindow->setTitle(title.c_str());
 }
-void HelloWorld::onBackendCreated() {
+
+void FullApp::onBackendCreated() {
     this->updateTitle();
     fWindow->show();
     fWindow->inval();
 }
-void HelloWorld::onPaint(SkSurface* surface) {
+
+void FullApp::onPaint(SkSurface* surface) {
     auto canvas = surface->getCanvas();
+    
     // Clear background
     canvas->clear(SK_ColorWHITE);
     SkPaint paint;
-    paint.setColor(SK_ColorRED);
-    // Draw a rectangle with red paint
-    SkRect rect = SkRect::MakeXYWH(10, 10, 128, 128);
-    canvas->drawRect(rect, paint);
-    // Set up a linear gradient and draw a circle
     {
         SkPoint linearPoints[] = { { 0, 0 }, { 300, 300 } };
         SkColor linearColors[] = { SK_ColorGREEN, SK_ColorBLACK };
@@ -75,32 +71,22 @@ void HelloWorld::onPaint(SkSurface* surface) {
                                                      SkTileMode::kMirror));
         paint.setAntiAlias(true);
         canvas->drawCircle(200, 200, 64, paint);
+        canvas->drawCircle(200, 200, 64, paint);
+        canvas->drawCircle(200, 200, 64, paint);
+
         // Detach shader
         paint.setShader(nullptr);
     }
-    // Draw a message with a nice black paint
-    SkFont font;
-    font.setSubpixel(true);
-    font.setSize(20);
-    paint.setColor(SK_ColorBLACK);
-    canvas->save();
-    static const char message[] = "Hello World ";
-    // Translate and rotate
-    canvas->translate(300, 300);
-    fRotationAngle += 0.2f;
-    if (fRotationAngle > 360) {
-        fRotationAngle -= 360;
-    }
-    canvas->rotate(fRotationAngle);
-    // Draw the text
-    canvas->drawSimpleText(message, strlen(message), SkTextEncoding::kUTF8, 0, 0, font, paint);
-    canvas->restore();
+
+    ImGui::ShowDemoWindow(&fShowImGuiTestWindow);
 }
-void HelloWorld::onIdle() {
+
+void FullApp::onIdle() {
     // Just re-paint continuously
     fWindow->inval();
 }
-bool HelloWorld::onChar(SkUnichar c, skui::ModifierKey modifiers) {
+
+bool FullApp::onChar(SkUnichar c, skui::ModifierKey modifiers) {
     if (' ' == c) {
         if (Window::kRaster_BackendType == fBackendType) {
             SkDebugf("No GPU backend configured\n");
