62c5d095c32e08d6aa94823404e24876a82e23b9
Matt Walters <mw2gd@virginia.edu>
2024-11-10 13:26:18 -0500

    working state

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
+# Replace the build file within the skia repository with 
+# custom build
 cp ./APPBUILD.gn ./ext/skia/BUILD.gn
+
+# Run gn within the skia repository
 cd ./ext/skia
-gn gen out/fullapp --args=' 
+gn gen ../../build/fullapp --args=' 
 is_official_build=false
 is_debug=false
 skia_enable_tools=true
@@ -8,4 +12,7 @@ skia_enable_skottie=false
 skia_enable_svg=false
 skia_use_zlib=false
 '
-ninja -C ./out/fullapp
+
+# Run ninja to output the application
+cd ../..
+ninja -C ./build/fullapp fullapp
