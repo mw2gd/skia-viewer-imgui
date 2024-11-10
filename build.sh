6ff8de5072ae70ee9920eb5dcc833ec9793fdda3
Matt Walters <mw2gd@virginia.edu>
2024-11-10 12:44:26 -0500

    working

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
-cmake -DAPPLE=TRUE -DCMAKE_BUILD_TYPE=Release -G Ninja -B ./build
-ninja -C ./build
+cp ./APPBUILD.gn ./ext/skia/BUILD.gn
+cd ./ext/skia
+gn gen out/fullapp --args=' 
+is_official_build=false
+is_debug=false
+skia_enable_tools=true
+skia_enable_skottie=false
+skia_enable_svg=false
+skia_use_zlib=false
+'
+ninja -C ./out/fullapp
