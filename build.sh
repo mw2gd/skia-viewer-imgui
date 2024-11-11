7f5a1b03bacf9d157ddc0a0bceb0922503ed1b5f
Matt Walters <mw2gd@virginia.edu>
2024-11-10 19:17:33 -0500

    Working build state

--- build.sh
+++ build.sh
@@ -4,15 +4,13 @@ cp ./APPBUILD.gn ./ext/skia/BUILD.gn
 
 # Run gn within the skia repository
 cd ./ext/skia
-gn gen ../../test/ --args='
+gn gen ../../out/ --args='
 is_debug=false
+is_official_build=false
 skia_enable_optimize_size=true
 
-skia_enable_tools=true
 skia_use_gl=true
-
 skia_enable_svg=false
-
 skia_use_system_libjpeg_turbo=false
 skia_use_system_libpng=false 
 skia_use_system_libwebp=false
@@ -20,13 +18,11 @@ skia_use_system_zlib=false
 skia_use_system_icu=false
 skia_use_system_harfbuzz=false
 skia_use_system_expat=false
-
-skia_compile_modules=true
 '
 
 # Run ninja to output the application
 cd ../..
-ninja -C ./test/ fullapp
+ninja -C ./out/ fullapp
 
 # Reduce application size by removing symbols
-strip ./test/fullapp
+strip ./out/fullapp
